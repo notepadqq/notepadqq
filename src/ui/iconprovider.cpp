52062883339112d7d125969ae65be712f9b3fb83
Daniele Di Sarli <danieleds0@gmail.com>
2014-09-18 12:05:51 +0200

    Fix IconProvider: wasn't picking up scalable icons (#64, #51)

--- src/ui/iconprovider.cpp
+++ src/ui/iconprovider.cpp
@@ -9,19 +9,21 @@ QIcon IconProvider::fromTheme(const QString &name)
     // FIXME Cache the icons
     QIcon icon = QIcon::fromTheme(name);
     if (icon.isNull()) {
-        QString basePath = ":/icons/default/%1/%2.png";
+        QString basePath = ":/icons/default/%1/%2.%3";
         QList<QPair<int, QString>> sizes {
                     QPair<int, QString>(16, "16x16"),
                     QPair<int, QString>(64, "64x64"),
                     QPair<int, QString>(128, "128x128"),
-                    QPair<int, QString>(512, "scalable"),
         };
 
         for (const QPair<int, QString> & size : sizes) {
-            icon.addFile(basePath.arg(size.second).arg(name),
+            icon.addFile(basePath.arg(size.second).arg(name).arg("png"),
                          QSize(size.first, size.first));
 
         }
+
+        icon.addFile(basePath.arg("scalable").arg(name).arg("svg"),
+                     QSize(512, 512));
     }
 
     return icon;
