8f55af6993de5c1418e3b541916426e59fafee80
Daniele Di Sarli <danieleds0@gmail.com>
2014-09-21 12:39:21 +0200

    Bump version

--- src/ui/include/notepadqq.h
+++ src/ui/include/notepadqq.h
@@ -31,7 +31,7 @@
 #include <QUrl>
 #include <QCommandLineParser>
 
-#define POINTVERSION "0.35.0" // major.minor.revision
+#define POINTVERSION "0.36.0" // major.minor.revision
 
 class Notepadqq
 {
