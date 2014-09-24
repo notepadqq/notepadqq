4d74a1617511362f485bf062004cd1d22f6bb9bb
Daniele Di Sarli <danieleds0@gmail.com>
2014-09-24 17:14:07 +0200

    XML detection

--- src/editor/classes/Languages.js
+++ src/editor/classes/Languages.js
@@ -657,7 +657,8 @@ var Languages = new function() {
             name: "XML",
             mode: "xml",
             mime: "application/xml",
-            fileExtensions: ["xml", "svg", "wxs", "wxl", "wsdl", "rss", "atom", "rdf", "xslt", "xsl", "xul", "xbl", "mathml", "config", "plist", "xaml", "qrc"]
+            fileExtensions: ["xml", "svg", "wxs", "wxl", "wsdl", "rss", "atom", "rdf", "xslt", "xsl", "xul", "xbl", "mathml", "config", "plist", "xaml", "qrc"],
+            firstNonBlankLine: [/^<\?xml /, /^<.+?>/]
         },
 
         "xquery": {
