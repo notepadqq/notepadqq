ca5ee60473198ee2c71635b1ea4b334d1172175d
Daniele Di Sarli <danieleds0@gmail.com>
2015-01-03 13:44:47 +0100

    Post-release version bump

--- src/ui/include/notepadqq.h
+++ src/ui/include/notepadqq.h
@@ -33,7 +33,7 @@
 #include <QList>
 #include "include/mainwindow.h"
 
-#define POINTVERSION "0.41.1" // major.minor.revision
+#define POINTVERSION "0.41.2" // major.minor.revision
 
 #define MIB_UTF_8 106
 
