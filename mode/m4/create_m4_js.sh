bd0159535e0c4ad47e02a84f9d7b3b2e191dac51
Marshall Banana <djcj@gmx.de>
2015-03-06 05:10:38 +0100

    m4: add AX_* macros from GNU Autoconf Archive (git://git.sv.gnu.org/autoconf-archive.git)

--- mode/m4/create_m4_js.sh
+++ mode/m4/create_m4_js.sh
@@ -2,7 +2,7 @@
 
 tmp=macros~
 
-cat macros.txt | tr '\n' ' ' | fold -s -w 71 | sed "s/^/    '/; s/$/' +/" > $tmp
+cat macros.txt autoconf-archive.txt | tr '\n' ' ' | fold -s -w 71 | sed "s/^/    '/; s/$/' +/" > $tmp
 sed -e "/@MACROS@/r $tmp" -e "/@MACROS@/d" m4.js.in > m4.js
 rm $tmp
 
