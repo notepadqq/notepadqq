09b1f1fa7c8a07227561b78fc08208ae3661483c
Marshall Banana <djcj@gmx.de>
2015-03-06 04:53:07 +0100

    m4: fold the text

--- mode/m4/create_m4_js.sh
+++ mode/m4/create_m4_js.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-macros="$(cat macros.txt | tr '\n' ' ')"
-sed "s/@MACROS@/$macros/" m4.js.in > m4.js
+
+tmp=macros~
+
+cat macros.txt | tr '\n' ' ' | fold -s -w 71 | sed "s/^/    '/; s/$/' +/" > $tmp
+sed -e "/@MACROS@/r $tmp" -e "/@MACROS@/d" m4.js.in > m4.js
+rm $tmp
+
