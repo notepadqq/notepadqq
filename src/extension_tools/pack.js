288a061e94e1ba8442f7d931b4039688e246c211
Daniele Di Sarli <danieleds0@gmail.com>
2015-06-10 21:23:48 +0200

    Update extension tools

--- src/extension_tools/pack.js
+++ src/extension_tools/pack.js
@@ -2,34 +2,59 @@ var os = require('os');
 var path = require('path');
 var crypto = require('crypto');
 var fs = require('fs');
-var cp = require('child_process');
 
-var ncp = require('ncp').ncp;
-var mkdirp = require('mkdirp');
-var rimraf = require('rimraf');
 var archiver = require('archiver');
+
+var sh = require('shelljs');
+sh.config.fatal = true;
 
 var project = process.argv[2];
 var npm = process.argv[3];
 
+// Check args
+if (process.argv.length !== 2+2) {
+    console.error("Usage: " + process.argv[1] + " projectFolder npmExecutable");
+    process.exit(1);
+}
+
 var MANIFEST_FILENAME = "nqq-manifest.json";
+
+function exec(name, args, options, callback) {
+    var cp = require('child_process');
+    var process = cp.spawn(name, args, options);
+
+    process.stdout.on('data', function (data) { console.log(data.toString()); });
+    process.stderr.on('data', function (data) { console.error(data.toString()); });
+
+    process.on('close', function (code) {
+        callback(code);
+    });
+}
+
+function clean() {
+    sh.rm('-rf', tmpdir);
+}
+
+function addBundleDepsToPackageJSON(fileName) {
+    var contents = fs.readFileSync(fileName);
+    var json = JSON.parse(contents);
+    if (json.dependencies) {
+        delete json.bundledDependencies; // Remove key with different spelling
+        json.bundleDependencies = Object.keys(json.dependencies);
+        fs.writeFileSync(fileName, JSON.stringify(json, null, 2));  
+    }
+}
 
 
 var tmpdir = path.join(os.tmpdir(), '_notepadqq_ext_' + crypto.randomBytes(4).readUInt32LE(0));
 var tmpdir_ext = path.join(tmpdir, 'ext');
 var tmpdir_pkg = path.join(tmpdir, 'pkg');
 
-mkdirp.sync(tmpdir);
-mkdirp.sync(tmpdir_pkg);
-mkdirp.sync(tmpdir_ext);
+sh.mkdir('-p', tmpdir);     // /tmp/_notepadqq_ext_xxxxx/
+sh.mkdir('-p', tmpdir_pkg); // Contains the notepadqq extension files
+sh.mkdir('-p', tmpdir_ext); // Contains a copy of the extension code
 
 console.log(tmpdir);
-
-
-function clean() {
-    rimraf.sync(tmpdir);
-}
-
 
 function buildArchive(callback) {
     var temporaryArchive = path.join(tmpdir, "temp.zip");
@@ -40,15 +65,8 @@ function buildArchive(callback) {
     
     output.on('close', function() {
         console.log('Written ' + archive.pointer() + ' bytes');
-        
-        ncp(temporaryArchive, definitiveArchive, function (err) {
-            if (err) {
-                throw err;
-            }
-            
-            callback(definitiveArchive);
-        });
-        
+        sh.cp('-f', temporaryArchive, definitiveArchive);
+        callback(definitiveArchive);
     });
 
     archive.on('error', function(err) {
@@ -73,42 +91,53 @@ function buildArchive(callback) {
 }
 
 
-
-// Copy extension folder
-ncp(project, tmpdir_ext, function (err) {
-    if (err) {
-        console.log("Error: " + err);
-        clean();
-        process.exit(1);
-    }
+function prepare_npm_package(callback) {
     
     // Remove notepadqq files
-    fs.unlinkSync(path.join(tmpdir_ext, MANIFEST_FILENAME));
+    sh.rm(path.join(tmpdir_ext, MANIFEST_FILENAME));
     
-    // FIXME Set bundleDependencies for the package
-    //rewritePackageJSON(path.join('node_modules', packageName, 'package.json'));
-    
-    var npmPack = cp.spawn(npm, ['pack', tmpdir_ext], { cwd: tmpdir_pkg });
-    
-    npmPack.stdout.on('data', function (data) {
-        console.log(data.toString());
-    });
-
-    npmPack.stderr.on('data', function (data) {
-        console.error(data.toString());
-    });
-
-    npmPack.on('close', function (code) {
+    // npm install
+    exec(npm, ['install'], { cwd: tmpdir_ext }, function(code) {
         if (code !== 0) {
-            console.log("Error executing npm pack: exit code ", code);
-            process.exit(0);
+            console.log("Error executing npm install: exit code ", code);
+            process.exit(1);
         }
         
+        addBundleDepsToPackageJSON(path.join(tmpdir_ext, 'package.json'));
+
+        callback();
+    });
+
+}
+
+function create_npm_package(callback) {
+    
+    // npm pack
+    exec(npm, ['pack', tmpdir_ext], { cwd: tmpdir_pkg }, function(code) {
+        if (code !== 0) {
+            console.log("Error executing npm pack: exit code ", code);
+            process.exit(1);
+        }
+
+        callback();
+    });
+    
+}
+
+
+
+// Copy extension folder to a temporary dir
+sh.cp('-r', path.join(project, '*'), path.join(project, '.*'), tmpdir_ext);
+
+prepare_npm_package(function() {
+
+    create_npm_package(function() {
+
         buildArchive(function(file) {
             console.log(file);
             clean();
         });
-        
+
     });
 
 });
