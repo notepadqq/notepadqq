6bcb282bf4f471899c678481e6409d1a64875fb4
Daniele Di Sarli <danieleds0@gmail.com>
2015-06-10 21:31:19 +0200

    Script to read manifest from zip file

---
+++ src/extension_tools/readmanifest.js
@@ -0,0 +1,18 @@
+var path = require('path');
+var fs = require('fs');
+
+var AdmZip = require('adm-zip');
+
+var package = process.argv[2];
+
+// Check args
+if (process.argv.length !== 2+1) {
+    console.error("Usage: " + process.argv[1] + " package");
+    process.exit(1);
+}
+
+var MANIFEST_FILENAME = "nqq-manifest.json";
+
+var zip = new AdmZip(package);
+var manifest = JSON.parse(zip.readAsText(MANIFEST_FILENAME));
+console.log(manifest);
