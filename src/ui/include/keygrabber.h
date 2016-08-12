3878c3f52d29f9f7e3d5b35f4256e63def931659
Justin Kinnaird <jkinnaird1991@gmail.com>
2016-08-12 15:10:19 -0400

    Warn users of keyboard shortcut conflicts with highlighting.

--- src/ui/include/keygrabber.h
+++ src/ui/include/keygrabber.h
@@ -6,10 +6,17 @@ class keyGrabber : public QTableWidget {
 Q_OBJECT
 public:
     keyGrabber(QWidget* parent = 0);
+    void checkConflicts();
 
 protected:
     void keyPressEvent(QKeyEvent* key);
+    void paintEvent(QPaintEvent* event);
 
+public slots:
+    void itemChanged(QTableWidgetItem* item);
+
+private:
+    QSet<int> m_conflicts;
 };
 
 #endif
