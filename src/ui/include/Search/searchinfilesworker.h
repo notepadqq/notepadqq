8f39a0740ee47af0e8b3b49468cc99b04b77b4b0
Justin Kinnaird <jkinnaird1991@gmail.com>
2016-08-29 02:40:40 -0400

    Search in Files: speed improvements (#256)

--- src/ui/include/Search/searchinfilesworker.h
+++ src/ui/include/Search/searchinfilesworker.h
@@ -3,6 +3,7 @@
 
 #include <QObject>
 #include <QMutex>
+#include <QRegularExpression>
 #include "include/Search/filesearchresult.h"
 #include "include/Search/searchhelpers.h"
 
@@ -24,6 +25,16 @@ signals:
     void finished(bool stopped);
     void error(QString string);
     void progress(QString file);
+
+    /**
+     * @brief Currently unused, meant for a live-updating version of search results
+     * @param `fileName`
+     * @param `line`
+     * @param `column`
+     * @param `lineContent`
+     * @param `matchLen`
+     */
+    //void matchFound(const QString &fileName, int line, int column, const QString &lineContent, int matchLen);
 
     /**
      * @brief Error reading a file. You can handle this signal
@@ -48,9 +59,62 @@ private:
     SearchHelpers::SearchOptions m_searchOptions;
     FileSearchResult::SearchResult m_result;
     QMutex m_resultMutex;
-    bool m_stop = false;
     QMutex m_stopMutex;
-    FileSearchResult::Result buildResult(const QRegularExpressionMatch &match, QString *content);
+    bool m_stop = false;
+    QRegularExpression m_regex;
+
+   /**
+    * @brief Build FileSearchResult::Result object using the parameters given.
+    * @param `line`
+    * @param `column`
+    * @param `absoluteColumn`
+    * @param `lineContent`
+    * @param `matchLen`
+    * @return `FileSearchResult::Result` object to be used for final search results.
+    */
+    FileSearchResult::Result buildResult(const int &line, 
+            const int &column, 
+            const int &absoluteColumn, 
+            const QString &lineContent, 
+            const int &matchLen);
+   /**
+    * @brief Perform a search using regular expression matching
+    * @param `fileName`
+    * @param `content`
+    * @return `FileSearchResult::FileResult` object containing all the matches
+    *         found in the file
+    */
+    FileSearchResult::FileResult searchRegExp(const QString &fileName, const QString &content);
+
+   /**
+    * @brief Perform a search using plain text.
+    * @param `fileName`
+    * @param `content`
+    * @return `FileSearchResult::FileResult` object containing all the matches
+    *         found in the file
+    */
+    FileSearchResult::FileResult searchPlainText(const QString &fileName, const QString &content);
+   /**
+    * @brief Boundary check `match` at `index` to ensure it isn't part of another word
+    * @param `index`
+    * @param `matchLength`
+    * @param `data`
+    * @return bool value based on results of the string test.
+    */
+    bool matchesWholeWord(const int &index, const int &matchLength, const QString &data);
+   /**
+    * @brief Build a list of positions in the given QString where a new line begins.
+    * @param `data`
+    * @return QVector<int> containing line beginning positions.
+    */
+    QVector<int> getLinePositions(const QString &data);
+
+   /*
+    * @brief Unescapes a string to allow searching for \n, \r, \t, etc.
+    * @param `data`
+    * @return QString with character sequences unescaped.
+    */
+    QString unescapeString(const QString &data);
 };
 
 #endif // SEARCHINFILESWORKER_H
