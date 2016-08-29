7a4504e079b477781d3794b4bb47ab3cf2f4377b
Justin Kinnaird <jkinnaird1991@gmail.com>
2016-08-29 02:40:40 -0400

    Implement single line regex searching + speed improvements. Adapted from kate.

--- src/ui/include/Search/searchinfilesworker.h
+++ src/ui/include/Search/searchinfilesworker.h
@@ -3,6 +3,7 @@
 
 #include <QObject>
 #include <QMutex>
+#include <QRegularExpression>
 #include "include/Search/filesearchresult.h"
 #include "include/Search/searchhelpers.h"
 
@@ -26,6 +27,16 @@ signals:
     void progress(QString file);
 
     /**
+     * @brief Currently unused, meant for a live-updating version of search results
+     * @param fileName
+     * @param line
+     * @param column
+     * @param lineContent
+     * @param matchLen
+     */
+    //void matchFound(const QString &fileName, int line, int column, const QString &lineContent, int matchLen);
+
+    /**
      * @brief Error reading a file. You can handle this signal
      *        and show a message box asking the user to abort/retry/ignore.
      *        Assign the result of the message box to "operation".
@@ -44,13 +55,21 @@ private:
     QString m_string;
     QString m_path;
     QStringList m_filters;
+    int m_matchCount;
     SearchHelpers::SearchMode m_searchMode;
     SearchHelpers::SearchOptions m_searchOptions;
     FileSearchResult::SearchResult m_result;
     QMutex m_resultMutex;
     bool m_stop = false;
     QMutex m_stopMutex;
-    FileSearchResult::Result buildResult(const QRegularExpressionMatch &match, QString *content);
+    QRegularExpression m_regex;
+    FileSearchResult::Result buildResult(int line, int column, const QString &lineContent, int matchLen);
+   /**
+    * @brief Perform a search using single line regular expression matching
+    * @param fileName
+    * @param data
+    */
+    FileSearchResult::FileResult searchSingleLineRegExp(const QString &fileName, QString content);
 };
 
 #endif // SEARCHINFILESWORKER_H
