a37817b1d9698eda7bf07c4ec1779e11e51cb7d2
Justin Kinnaird <jkinnaird1991@gmail.com>
2016-09-09 05:43:54 -0400

    More documentation stuff.

--- src/ui/include/Search/searchstring.h
+++ src/ui/include/Search/searchstring.h
@@ -5,8 +5,26 @@
 
 class SearchString {
 public:
+   /*
+    * @brief Build a raw version of `data` to be used for the search.
+    * @param `data`:          The string to be worked on.
+    * @param `searchMode`:    The search mode to be used.
+    * @param `searchOptions`: The search options to be used.
+    * @return `QString`:      Converted string based on `searchMode` and `searchOptions`
+    */
     static QString toRaw(const QString &data, const SearchHelpers::SearchMode &searchMode, const SearchHelpers::SearchOptions &searchOptions);
+   /*
+    * @brief Convert string to its regex counterpart.
+    * @param `data`:           The string to be worked on.
+    * @param `matchWholeWord`: Whether to add boundary checks to converted string.
+    * @return `QString`:       String converted for use in regex operations.
+    */
     static QString toRegex(const QString &data, bool matchWholeWord);
+   /*
+    * @brief Unescape escape sequences in `data`
+    * @param `data`:     The string to be worked on.
+    * @return `QString`: `data` with escape sequences unescaped.
+    */
     static QString unescape(const QString &data);
 };
 
