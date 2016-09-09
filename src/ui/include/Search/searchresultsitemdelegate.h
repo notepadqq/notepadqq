6f0806f3ef5781298391b9a629770d036d0dcbf0
Justin Kinnaird <jkinnaird1991@gmail.com>
2016-09-09 05:51:02 -0400

    Document searchresultsitemdelegate.h

--- src/ui/include/Search/searchresultsitemdelegate.h
+++ src/ui/include/Search/searchresultsitemdelegate.h
@@ -1,11 +1,12 @@
 #ifndef SEARCHRESULTSITEMDELEGATE_H
 #define SEARCHRESULTSITEMDELEGATE_H
+
+#include "include/Search/filesearchresult.h"
 #include <QSize>
 #include <QPainter>
 #include <QStyleOptionViewItemV4>
 #include <QStyledItemDelegate>
 #include <QStandardItem>
-#include "include/Search/filesearchresult.h"
 
 class SearchResultsItemDelegate : public QStyledItemDelegate
 {
@@ -21,13 +22,30 @@ public:
         ResultTypeFile,
         ResultTypeMatch
     };
-
+   
+   /*
+    * @brief Get the current `index` row item type.
+    * @return `SearchResultsItemDelegate::ResultType`: The result type of `index`.
+    */
     static SearchResultsItemDelegate::ResultType rowItemType(const QModelIndex &index);
 
+   /*
+    * @brief Populate result items.
+    * @param `item`:             The item to populate.
+    * @param `result`:           The result to populate `item` with.
+    * @param `matches`:          Total number of matches within a file.
+    * @param `totalFileMatches`: Total number of matches from a search.
+    * @param `totalFiles`:       Total number of files matched within.
+    */
     static void fillResultRowItem(QStandardItem *item, const FileSearchResult::Result &result);
     static void fillFileResultRowItem(QStandardItem *item, const FileSearchResult::FileResult &fileResult, const int matches);
     static void fillSearchResultRowItem(QStandardItem *item, const FileSearchResult::SearchResult &searchResult, const int totalFileMatches, const int totalFiles);
 
+   /*
+    * @brief Retrieve result data.
+    * @param `index`: The index to use for retrieving result data.
+    * @return `FileSearchResult::Result/FileResult`: The data at `index`.
+    */
     static FileSearchResult::Result resultRowData(const QModelIndex &index);
     static FileSearchResult::FileResult fileResultRowData(const QModelIndex &index);
 
