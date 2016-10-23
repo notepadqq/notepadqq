e4dc85c70e5e4d51c50037bb5ebcbea116bf3213
Justin Kinnaird <jkinnaird1991@gmail.com>
2016-10-22 20:02:55 -0400

    Experimental thing to make run function normally.

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -1975,52 +1975,6 @@ void MainWindow::modifyRunCommands()
     }
 }
 
-QStringList parseCombinedArgString(const QString &program)
-{
-    QStringList args;
-    QString tmp;
-    int quoteCount = 0;
-    bool inQuote = false;
-    // handle quoting. tokens can be surrounded by double quotes
-    // "hello world". three consecutive double quotes represent
-    // the quote character itself.
-    for (int i = 0; i < program.size(); ++i)
-    {
-        if (program.at(i) == QLatin1Char('"'))
-        {
-            ++quoteCount;
-            if (quoteCount == 3)
-            {
-                // third consecutive quote
-                quoteCount = 0;
-                tmp += program.at(i);
-            }
-            continue;
-        }
-        if (quoteCount)
-        {
-            if (quoteCount == 1)
-                inQuote = !inQuote;
-            quoteCount = 0;
-        }
-        if (!inQuote && program.at(i).isSpace())
-        {
-            if (!tmp.isEmpty())
-            {
-                args += tmp;
-                tmp.clear();
-            }
-        }
-        else
-        {
-            tmp += program.at(i);
-        }
-    }
-    if (!tmp.isEmpty())
-        args += tmp;
-    return args;
-}
-
 void MainWindow::runCommand()
 {
     QAction *a = qobject_cast<QAction*>(sender());
