f123d27e4b9e5ba38cc4262ba61dbeb00ed67118
Daniele Di Sarli <danieleds0@gmail.com>
2017-03-12 16:39:47 +0100

    Use arrow functions to avoid _this alias

--- src/editor/classes/UiDriver.js
+++ src/editor/classes/UiDriver.js
@@ -1,16 +1,15 @@
 class CommDriver {
     constructor() {
-        var _this = this;
         this.handlers = [];
-        new QWebChannel(qt.webChannelTransport, function (_channel) {
-            _this.proxy = _channel.objects.cpp_ui_driver;
-            _this.proxy.sendEditorEvent("J_EVT_PROXY_INIT", 0);
-            _this.proxy.sendMsgInternal.connect(function(msg, data) {
-                _this.messageReceived(msg, data);
+        new QWebChannel(qt.webChannelTransport, _channel => {
+            this.proxy = _channel.objects.cpp_ui_driver;
+            this.proxy.sendEditorEvent("J_EVT_PROXY_INIT", 0);
+            this.proxy.sendMsgInternal.connect((msg, data) => {
+                this.messageReceived(msg, data);
             });
         });
     }
-    
+
     registerEventHandler(msg, handler) {
         if (this.handlers[msg] === undefined)
             this.handlers[msg] = [];
@@ -20,7 +19,7 @@ class CommDriver {
     setReturnData(data) {
         this.proxy.result = data;
     }
-    
+
     messageReceived(msg, data) {
         // Only one of the handlers (the last that gets
         // called) can return a value. So, to each handler
@@ -31,7 +30,7 @@ class CommDriver {
                 prevReturn = handler(msg, data, prevReturn);
             });
         }
-        
+
         if(prevReturn !== undefined) {
             this.setReturnData(prevReturn);
         }
