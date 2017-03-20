1720507ef6874fa091cbdcde1aa88608ff5b4422
Teklad <jkinnaird1991@gmail.com>
2017-03-20 05:16:19 -0500

    Remove old work-around for a previous CodeMirror version.

--- src/editor/classes/Languages.js
+++ src/editor/classes/Languages.js
@@ -4,27 +4,18 @@ var Languages = new function() {
     this.languages = undefined;
 
     this.setLanguage = function(editor, lang) {
-        if (lang.mime !== undefined && lang.mime !== null && lang.mime !== "")
+        if (lang.mime !== undefined && lang.mime !== null && lang.mime !== "") {
             editor.setOption("mode", lang.mime);
-        else
+        }else {
             editor.setOption("mode", lang.mode);
-            
-        m_currentLanguage = lang.id;
-
-        if (editor.getOption("foldGutter") == true) {
-            editor.setOption("foldGutter", false);
-            editor.setOption("foldGutter", true);
         }
 
+        m_currentLanguage = lang.id;
+        editor.refresh();
         evhook.onLanguageChange(UiDriver.proxy, editor, lang);
     }
     
     this.currentLanguage = function() {
         return m_currentLanguage;
     }
-    
-    function endsWith(str, suffix) {
-        return str.indexOf(suffix, str.length - suffix.length) !== -1;
-    }
-
 }
