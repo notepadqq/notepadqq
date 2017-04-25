1b8d7c0dc2bf663b1a17b75250eb55d9a79f9dc3
Quinton Erasmus <qcerasmus@gmail.com>
2017-04-25 20:53:15 +0200

    %directory% in run command.
    fixes #433

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -2041,7 +2041,7 @@ void MainWindow::runCommand()
         cmd.replace("\%fullpath\%", url.toString(QUrl::None));
         cmd.replace("\%path\%", url.path(QUrl::FullyEncoded));
         cmd.replace("\%filename\%", url.fileName(QUrl::FullyEncoded));
-        QString directory = url.toString().replace("file://", "").replace(url.fileName(), "");
+        QString directory = QFileInfo(url.toLocalFile()).absolutePath();
         cmd.replace("\%directory\%", directory);
     }
     if(!selection.first().isEmpty()) {
