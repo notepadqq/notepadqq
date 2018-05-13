b2842cc59af1f605fd7e6f02bdc6b4996dcdb505
Teklad <jkinnaird1991@gmail.com>
2018-05-12 23:15:25 -0500

    refac: Try using a matrix to parallel jobs.

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
-./configure && make && ./src/ui-tests/ui-tests
+check_format()
+{
+    gcf_cmd="$(which git-clang-format-3.9) --binary $(which clang-format-3.9)"
+    if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+        base_commit="HEAD^"
+        echo "Running clang-format against parent commit $(git rev-parse $base_commit)"
+    else
+        base_commit="$TRAVIS_BRANCH"
+        echo "Running clang-format against branch $base_commit, with hash $(git rev-parse $base_commit)"
+    fi
+    output="$($gcf_cmd --commit $base_commit --diff --extensions 'cpp,h')"
+    if [ "$output" == "no modified files to format" ] || [ "$output" == "clang-format did not modify any files" ]; then
+        echo "clang-format passed."
+        exit 0
+    else
+        echo "clang-format failed:"
+        echo "$output"
+        exit 1
+    fi
+}
+
+compile()
+{
+    ./configure && make && ./src/ui-tests/ui-tests
+}
+
+
+if [ "$BUILD_TYPE" == "FORMAT"]; then
+    check_format
+else
+    compile
+fi
