97afeb0346acce6282a3a392faa231a26a75b3fe
Daniele Di Sarli <danieleds0@gmail.com>
2019-09-02 18:23:25 +0200

    Fix macOS bundle by correctly referencing paths to Qt libraries. Fixes #790.

--- .travis/macos-script.sh
+++ .travis/macos-script.sh
@@ -17,8 +17,20 @@ deploy()
     compile || return 1
 
     cd out/release
-    macdeployqt notepadqq.app -dmg
-    mv notepadqq.dmg notepadqq-${NQQ_VERSION}.dmg || return 1
+    #macdeployqt notepadqq.app  # <-- this would be easy, but it is broken with QtWebEngine
+
+    macdeployqt notepadqq.app -executable=notepadqq.app/Contents/MacOS/notepadqq || return 1
+    macdeployqt notepadqq.app -executable=notepadqq.app/Contents/Frameworks/QtWebEngineCore.framework/Versions/5/Helpers/QtWebEngineProcess.app/Contents/MacOS/QtWebEngineProcess || return 1
+
+    git clone --depth=1 https://github.com/aurelien-rainone/macdeployqtfix.git
+    python macdeployqtfix/macdeployqtfix.py notepadqq.app/Contents/MacOS/notepadqq /usr/local/Cellar/qt/5.* || return 1
+    python macdeployqtfix/macdeployqtfix.py notepadqq.app/Contents/Frameworks/QtWebEngineCore.framework/Versions/5/Helpers/QtWebEngineProcess.app/Contents/MacOS/QtWebEngineProcess /usr/local/Cellar/qt/5.* || return 1
+
+    # Manually create the DMG
+    mkdir dmgfolder
+    mv notepadqq.app dmgfolder
+    ln -s /Applications ./dmgfolder/Applications
+    hdiutil create -fs HFS+ -format UDBZ -srcfolder ./dmgfolder -volname Notepadqq notepadqq-${NQQ_VERSION}.dmg || return 1
 }
 
 if [ "$NQQ_BUILD_TYPE" == "DEPLOY" ]; then
