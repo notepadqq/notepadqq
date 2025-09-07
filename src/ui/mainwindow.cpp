087c8ddc564b810a28dd2c983a6dc1f923891c83
Richard Allen <rsaxvc@rsaxvc.net>
2025-09-06 22:22:17 -0500

    Replace deprecated mousewheel delta() -> angleDelta().y()
    mainwindow.cpp: In member function ‘void MainWindow::on_editorMouseWheel(EditorTabWidget*, int, QWheelEvent*)’:
    mainwindow.cpp:1671:31: warning: ‘int QWheelEvent::delta() const’ is deprecated: Use angleDelta() [-Wdeprecated-declarations]
     1671 |         qreal diff = ev->delta() / 120;

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -1668,7 +1668,7 @@ void MainWindow::on_editorMouseWheel(EditorTabWidget *tabWidget, int tab, QWheel
 {
     if (QApplication::keyboardModifiers() & Qt::ControlModifier) {
         qreal curZoom = tabWidget->editor(tab)->zoomFactor();
-        qreal diff = ev->delta() / 120;
+        qreal diff = ev->angleDelta().y() / 120;
         diff /= 10;
 
         // Increment/Decrement zoom factor by 0.1 at each step.
